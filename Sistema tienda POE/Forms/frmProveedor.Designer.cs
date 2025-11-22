de3de7a255eb9cabb2c8eeb1663e4cbcdcc4faf6
Wippomonster <100671672+Wippomonster@users.noreply.github.com>
2025-11-21 20:51:09 -0600

    Refactor frmProveedor layout

--- Sistema tienda POE/Forms/frmProveedor.Designer.cs
+++ Sistema tienda POE/Forms/frmProveedor.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Sistema_tienda_POE.Forms
+namespace Sistema_tienda_POE.Forms
 {
     partial class frmProveedor
     {
@@ -45,7 +45,6 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.lbInfo = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.btnCancelar = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProveedores)).BeginInit();
             this.panel1.SuspendLayout();
@@ -58,14 +57,14 @@
             this.dgvProveedores.AllowUserToDeleteRows = false;
             this.dgvProveedores.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvProveedores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvProveedores.Location = new System.Drawing.Point(12, 544);
+            this.dgvProveedores.Location = new System.Drawing.Point(82, 351);
             this.dgvProveedores.MultiSelect = false;
             this.dgvProveedores.Name = "dgvProveedores";
             this.dgvProveedores.ReadOnly = true;
             this.dgvProveedores.RowHeadersWidth = 51;
             this.dgvProveedores.RowTemplate.Height = 24;
             this.dgvProveedores.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvProveedores.Size = new System.Drawing.Size(1118, 480);
+            this.dgvProveedores.Size = new System.Drawing.Size(896, 266);
             this.dgvProveedores.TabIndex = 0;
             this.dgvProveedores.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvProveedores_CellClick);
             // 
@@ -75,7 +74,7 @@
             this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(15, 6);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 24);
+            this.label1.Size = new System.Drawing.Size(52, 19);
             this.label1.TabIndex = 1;
             this.label1.Text = "Nombre:";
             // 
@@ -83,14 +82,14 @@
             // 
             this.txtNombre.Location = new System.Drawing.Point(82, 6);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(337, 30);
+            this.txtNombre.Size = new System.Drawing.Size(337, 26);
             this.txtNombre.TabIndex = 0;
             // 
             // txtNIT
             // 
             this.txtNIT.Location = new System.Drawing.Point(82, 54);
             this.txtNIT.Name = "txtNIT";
-            this.txtNIT.Size = new System.Drawing.Size(169, 30);
+            this.txtNIT.Size = new System.Drawing.Size(169, 26);
             this.txtNIT.TabIndex = 1;
             // 
             // label2
@@ -99,7 +98,7 @@
             this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(15, 54);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 24);
+            this.label2.Size = new System.Drawing.Size(28, 19);
             this.label2.TabIndex = 3;
             this.label2.Text = "NIT:";
             // 
@@ -107,7 +106,7 @@
             // 
             this.txtTelefono.Location = new System.Drawing.Point(82, 102);
             this.txtTelefono.Name = "txtTelefono";
-            this.txtTelefono.Size = new System.Drawing.Size(169, 30);
+            this.txtTelefono.Size = new System.Drawing.Size(169, 26);
             this.txtTelefono.TabIndex = 2;
             // 
             // label3
@@ -116,7 +115,7 @@
             this.label3.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(15, 102);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 24);
+            this.label3.Size = new System.Drawing.Size(56, 19);
             this.label3.TabIndex = 5;
             this.label3.Text = "Teléfono:";
             // 
@@ -124,7 +123,7 @@
             // 
             this.txtDireccion.Location = new System.Drawing.Point(82, 148);
             this.txtDireccion.Name = "txtDireccion";
-            this.txtDireccion.Size = new System.Drawing.Size(337, 30);
+            this.txtDireccion.Size = new System.Drawing.Size(337, 26);
             this.txtDireccion.TabIndex = 3;
             // 
             // label4
@@ -133,7 +132,7 @@
             this.label4.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(15, 149);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(76, 24);
+            this.label4.Size = new System.Drawing.Size(61, 19);
             this.label4.TabIndex = 7;
             this.label4.Text = "Dirección:";
             // 
@@ -144,14 +143,14 @@
             this.chkEstado.CheckState = System.Windows.Forms.CheckState.Checked;
             this.chkEstado.Location = new System.Drawing.Point(494, 9);
             this.chkEstado.Name = "chkEstado";
-            this.chkEstado.Size = new System.Drawing.Size(88, 29);
+            this.chkEstado.Size = new System.Drawing.Size(71, 24);
             this.chkEstado.TabIndex = 4;
             this.chkEstado.Text = "Activo";
             this.chkEstado.UseVisualStyleBackColor = true;
             // 
             // btnGuardar
             // 
-            this.btnGuardar.Location = new System.Drawing.Point(494, 66);
+            this.btnGuardar.Location = new System.Drawing.Point(494, 78);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(112, 43);
             this.btnGuardar.TabIndex = 5;
@@ -161,7 +160,7 @@
             // 
             // btnEliminar
             // 
-            this.btnEliminar.Location = new System.Drawing.Point(628, 66);
+            this.btnEliminar.Location = new System.Drawing.Point(629, 78);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(112, 43);
             this.btnEliminar.TabIndex = 6;
@@ -171,7 +170,7 @@
             // 
             // btnLimpiar
             // 
-            this.btnLimpiar.Location = new System.Drawing.Point(764, 66);
+            this.btnLimpiar.Location = new System.Drawing.Point(764, 78);
             this.btnLimpiar.Name = "btnLimpiar";
             this.btnLimpiar.Size = new System.Drawing.Size(112, 43);
             this.btnLimpiar.TabIndex = 7;
@@ -183,7 +182,7 @@
             // 
             this.txtIdProveedor.Location = new System.Drawing.Point(426, 6);
             this.txtIdProveedor.Name = "txtIdProveedor";
-            this.txtIdProveedor.Size = new System.Drawing.Size(60, 30);
+            this.txtIdProveedor.Size = new System.Drawing.Size(60, 26);
             this.txtIdProveedor.TabIndex = 12;
             this.txtIdProveedor.Text = "0";
             this.txtIdProveedor.Visible = false;
@@ -192,10 +191,10 @@
             // 
             this.panel1.Controls.Add(this.lbInfo);
             this.panel1.Controls.Add(this.panel2);
-            this.panel1.Location = new System.Drawing.Point(44, 134);
+            this.panel1.Location = new System.Drawing.Point(13, 69);
             this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1118, 402);
+            this.panel1.Size = new System.Drawing.Size(1038, 274);
             this.panel1.TabIndex = 13;
             // 
             // lbInfo
@@ -205,13 +204,12 @@
             this.lbInfo.Location = new System.Drawing.Point(369, 0);
             this.lbInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbInfo.Name = "lbInfo";
-            this.lbInfo.Size = new System.Drawing.Size(332, 48);
+            this.lbInfo.Size = new System.Drawing.Size(265, 39);
             this.lbInfo.TabIndex = 13;
             this.lbInfo.Text = "Gestion de Proveedores";
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.btnCancelar);
             this.panel2.Controls.Add(this.txtIdProveedor);
             this.panel2.Controls.Add(this.btnLimpiar);
             this.panel2.Controls.Add(this.btnEliminar);
@@ -228,36 +226,26 @@
             this.panel2.Location = new System.Drawing.Point(69, 62);
             this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(982, 306);
+            this.panel2.Size = new System.Drawing.Size(896, 185);
             this.panel2.TabIndex = 13;
-            // 
-            // btnCancelar
-            // 
-            this.btnCancelar.Location = new System.Drawing.Point(494, 115);
-            this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(112, 42);
-            this.btnCancelar.TabIndex = 15;
-            this.btnCancelar.Text = "Cancelar";
-            this.btnCancelar.UseVisualStyleBackColor = true;
-            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(45)))), ((int)(((byte)(11)))));
             this.panel3.Location = new System.Drawing.Point(-24, 1);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1183, 125);
+            this.panel3.Size = new System.Drawing.Size(1090, 60);
             this.panel3.TabIndex = 14;
             // 
             // frmProveedor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(242)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(1142, 997);
+            this.ClientSize = new System.Drawing.Size(1064, 655);
+            this.Controls.Add(this.dgvProveedores);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.dgvProveedores);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "frmProveedor";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -292,6 +280,5 @@
         private System.Windows.Forms.Label lbInfo;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Button btnCancelar;
     }
 }
